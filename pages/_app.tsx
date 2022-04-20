f50322db789837ff543c74c0518ad76b2e75d16a
iinoten <nteten0313@icloud.com>
2022-04-20 22:56:38 +0900

    コメントを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    // RecoilRootで囲むことでアプリ全体をRecoilの管理の配下に置く
     <RecoilRoot>
       <Component {...pageProps} />
     </RecoilRoot>
